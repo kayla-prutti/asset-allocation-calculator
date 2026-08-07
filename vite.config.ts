ba96212fb5771d7c4d9c3d587a6911284feed5c6
Kayla Pruttisarikorn <kwan_u_day@hotmail.com>
2026-08-06 20:11:08 -0400

    ci: deploy app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS
+    ? "/asset-allocation-calculator/"
+    : "/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
